a61e763f97500f955864419694f89332ae4e9c25
AnastasiaMoshkova <nast.moshkova@yandex.ru>
2018-05-24 20:08:35 +0300

    дз8

--- d_task/taskTracker/models.py
+++ d_task/taskTracker/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.utils import timezone
+import uuid
 
 
 class Tasks(models.Model):
-    id = models.AutoField(primary_key=True)
-    key = models.CharField(max_length=40)
+    STATUS_TYPES = (
+        ('ADD', 'add task'),
+        ('GET', 'get task'),
+        ('FIN', 'finished task'),
+    )
+    key = models.CharField(max_length=40,unique=True)
     name = models.CharField(max_length=20)
-    status = models.CharField(max_length=20)
+    status = models.CharField(max_length=3,choices=STATUS_TYPES,default='ADD')
     user_id=models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True,null=True)
     parent_id = models.ForeignKey("self", on_delete=models.CASCADE, blank=True,null=True)
 
--- d_task/taskTracker/views.py
+++ d_task/taskTracker/views.py
@@ -5,6 +5,31 @@ from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 import os
 import json
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def add(request):
+
+    if request.method == 'POST':
+        datas=request.body
+        s=datas.decode('utf-8')
+        data = json.loads(s)
+        name = data.get('name')
+        key = data.get('key')
+        user_id = data.get('user_id')
+        parent_id = data.get('parent_id')
+        task = Tasks.objects.create()
+        task.name = name
+        task.key = key
+        task.parent_id = parent_id
+        task.user_id = user_id
+        task.status = 'ADD'
+        task.save()
+
+        return HttpResponse('create success')
+    else:
+        return HttpResponse('bad request', status=400)
+
 
 def task(request):
     tasks_tr = Tasks.objects.all()
@@ -14,29 +39,11 @@ def task(request):
     return render(request, 'taskTracker/task.html', {'tasks': tasks_tr,'users':users_tr })
 
 
-def add(request, json_file):
-
-    directory="C:\\Users\\Nastya\\Pictures\\techno_python"
-
-    with open(os.path.join(directory, json_file +'.json'), 'r', encoding='utf-8') as fh:  # открываем файл на чтение
-        json_content = json.load(fh)  # загружаем из файла данные в словарь data
-
-    task = Tasks.objects.create()
-    task.name=json_content['name']
-    task.key=json_content['key']
-    task.parent_id = json_content['parent_id']
-    task.status='add'
-    task.save()
-
-    return render(request, 'taskTracker/task.html', {'file': json_content['name']})
-
-
-
 def status(request,key):
 
     tasks = Tasks.objects.filter(key=key)
     for task in tasks:
-        task.status="finished"
+        task.status="FIN"
         task.save()
 
 
@@ -47,9 +54,8 @@ def get_task(request,key,user):
 
     user_name = get_user_model().objects.get(username=user)
     for task in tasks:
-        #task.user_id = user_name[0].username
         task.user_id =user_name
-        task.status = "get"
+        task.status = "GET"
         task.save()
     return render(request, 'taskTracker/task.html', {'file': user})
 
@@ -64,10 +70,10 @@ def user_tasks(request,id):
 def list_task(request,id):
     tasks_tr = Tasks.objects.filter(id=id)
     userid=tasks_tr[0].user_id
-    taskname = tasks_tr[0].name
     if userid:
         users_tr = get_user_model().objects.filter(username=userid)
     else:
         users_tr =[]
     child_tr=Tasks.objects.filter(parent_id=id)
     return render(request, 'taskTracker/task_list.html', {'tasks': tasks_tr,'users':users_tr, 'childrens':child_tr})
+
